459959ad31a709629f057f5d4855bd96e62257cf
Alessandro Biessek <alessandro@biessek.rocks>
2018-07-23 15:43:37 -0300

    Adicionado implementação salvamento no repositório de categorias

--- app/src/main/java/rocks/biessek/testemeuspedidos/data/local/ProductCategoriesLocalDataSource.kt
+++ app/src/main/java/rocks/biessek/testemeuspedidos/data/local/ProductCategoriesLocalDataSource.kt
@@ -5,9 +5,8 @@ import rocks.biessek.testemeuspedidos.data.model.ProductCategory
 
 class ProductCategoriesLocalDataSource(private val categoriesDao: ProductCategoriesDao) : CategoriesDataSource {
 
-    override fun saveCategory(category: ProductCategory): Boolean {
-        return false
-    }
+    override fun saveCategory(category: ProductCategory): Boolean =
+            categoriesDao.saveProductCategory(category) > 0L
 
     override fun loadCategories(): List<ProductCategory> = categoriesDao.getAllCategories()
 }
